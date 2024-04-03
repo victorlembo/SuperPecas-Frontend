66f6dde0c9e31aa16b6bd0966818e0544e9cdb46
Victor Lembo <victor.lembo@pagmob.com.br>
2024-04-02 22:01:19 -0300

    ajuste route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarrosComponent } from './carros/carros.component';
-import { PecasComponent } from './pecas/pecas.component'; 
+import { PecasComponent } from './pecas/pecas.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'carros', component: CarrosComponent },
-  { path: 'pecas', component: PecasComponent }, 
-  { path: 'home', component: HomeComponent }, 
+  { path: 'pecas', component: PecasComponent },
 ];
 
 @NgModule({
